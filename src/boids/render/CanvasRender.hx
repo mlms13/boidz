a4c6a67e779977839a9417a5200652ab7a2fbd41
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-04 22:10:55 -0700

    minor

--- src/boids/render/CanvasRender.hx
+++ src/boids/render/CanvasRender.hx
@@ -5,23 +5,35 @@ import js.html.CanvasRenderingContext2D;
 import js.html.CanvasElement;
 
 class CanvasRender extends BaseRule {
-    var canvas : CanvasElement;
-    var ctx : CanvasRenderingContext2D;
-    public function new(canvas : CanvasElement) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext2d();
-    }
-    override public function pre() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
-    }
+  var canvas : CanvasElement;
+  var ctx : CanvasRenderingContext2D;
+  public function new(canvas : CanvasElement) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext2d();
+  }
 
-    override public function post() {
-        ctx.stroke();
-    }
+  override public function pre() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
 
-    override public function modify(b:Boid):Void {
-        ctx.moveTo(b.px, b.py);
-        ctx.lineTo(b.px - b.vx, b.py - b.vy);
-    }
+// OR
+//    ctx.fillStyle = "rgba(255,255,255,1)";
+//    ctx.fillRect(0, 0, canvas.width, canvas.height);
+//    ctx.fillStyle = "#000";
+  }
+
+  override public function post() {
+    ctx.stroke();
+
+// OR
+//    ctx.fill();
+  }
+
+  override public function modify(b:Boid):Void {
+    ctx.moveTo(b.px, b.py);
+    ctx.lineTo(b.px - b.vx, b.py - b.vy);
+
+// OR
+//    ctx.fillRect(b.px, b.py, 1, 1);
+  }
 }
