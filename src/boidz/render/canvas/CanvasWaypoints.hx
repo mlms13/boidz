ef85cebf8b76c6f5452c8c4c7ec3f6f4cd7a006d
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-12-14 11:23:26 -0700

    minor waypoints improvements

--- src/boidz/render/canvas/CanvasWaypoints.hx
+++ src/boidz/render/canvas/CanvasWaypoints.hx
@@ -35,16 +35,16 @@ class CanvasWaypoints implements IRenderable<CanvasRender> {
 
     for(goal in waypoints.goals) {
       ctx.strokeStyle = "#CCCCCC";
-      ctx.lineTo(goal[0], goal[1]);
+      ctx.lineTo(goal.x, goal.y);
       ctx.stroke();
 
       ctx.beginPath();
       ctx.strokeStyle = "";
-      ctx.arc(goal[0], goal[1], waypoints.radius, 0, 2 * Math.PI, false);
+      ctx.arc(goal.x, goal.y, waypoints.radius, 0, 2 * Math.PI, false);
       ctx.fill();
 
       ctx.beginPath();
-      ctx.moveTo(goal[0], goal[1]);
+      ctx.moveTo(goal.x, goal.y);
     }
   }
 }
